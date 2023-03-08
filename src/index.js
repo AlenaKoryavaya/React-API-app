89d9dfecc9180a2a9bf12ada08c0b20a8d5692f9
Alena Koryavaya <elena.koryavaya@gmail.com>
2023-03-08 13:25:29 +0300

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter as Router } from "react-router-dom";
+
 import App from "./components/app/App";
 
 import "./style/style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <Router>
+        <App />
+    </Router>
+);
