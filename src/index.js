80f741f834a2346c6abbf6ca8ea0f2434f600ff9
Alena Koryavaya <elena.koryavaya@gmail.com>
2023-03-08 13:36:35 +0300

    fix: deleted extra router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter as Router } from "react-router-dom";
 
 import App from "./components/app/App";
 
 import "./style/style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <Router>
-        <App />
-    </Router>
-);
+root.render(<App />);
